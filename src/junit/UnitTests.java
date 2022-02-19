12bbbc09b1b912c185c674021df35806b5050e4c
MRegnard <62838312+MRegnard@users.noreply.github.com>
2022-02-19 17:46:12 +0100

    Update UnitTests.java
    Vérification du nombre d'agents après initialisation

--- src/junit/UnitTests.java
+++ src/junit/UnitTests.java
@@ -1,9 +1,35 @@
 package junit;
 
-// import static org.junit.Assert.*;
-// import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.Map;
+import main.* ;
 
 public class UnitTests {
+
+  private static String FILE_CONF = "conf.json" ;
+  private static int EXPECTED_NB_BROKER = 1 ;
+  private static int EXPECTED_NB_CONSUMERS = 1 ;
+  private static int EXPECTED_NB_PRODUCERS = 2 ;
+  private static int EXPECTED_NB_PROSUMERS = 1 ;
   
+  private Configuration conf ;
+  
+  @Before
+  public void initialize() {
+      conf = new Configuration(FILE_CONF) ; 
+  }
+  
+  
+  @Test
+  public void checkNbAgents() {
+      int expectedNbAgents = EXPECTED_NB_BROKER + EXPECTED_NB_CONSUMERS + EXPECTED_NB_PRODUCERS + EXPECTED_NB_PROSUMERS ;
+      assertEquals(conf.getAgents().size() , expectedNbAgents) ;
+    
+      assertNotNull(GraphicHelper.getBroker()) ;
+      assertEquals(GraphicHelper.getConsumers().size() , EXPECTED_NB_CONSUMERS) ;
+      assertEquals(GraphicHelper.getProducers().size() , EXPECTED_NB_PRODUCERS) ;
+      assertEquals(GraphicHelper.getProsumers().size() , EXPECTED_NB_PROSUMERS) ;
+  }
   
 }
